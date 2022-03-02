5f3c6ffeab5006afb9e5cce25b84389c24e70924
Mansi Vadane <52355123+Mansivadane@users.noreply.github.com>
2022-03-02 22:58:19 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,69 @@
+m=int(input("M="))
+s=int(input("S="))
+
+print("Arithmetic Operators")
+print(s+m)
+print(s-m)
+print(s*m)
+print(s/m)
+print(s%m)
+print(s**m)
+print(s//m)
+
+print("Assignment Operators")
+
+x=int(input("X="))
+x=x
+print(x)
+
+x += 3
+print(x)
+
+x -= 3
+print(x)
+
+x *= 3
+print(x)
+
+x /= 3
+print(x)
+
+x %= 3
+print(x)
+
+x //= 3
+print(x)
+
+x **= 3
+print(x)
+
+x = 5
+x &= 3
+print(x)
+
+x = 5
+x |= 3
+print(x)
+
+x = 5
+x ^= 3
+print(x)
+
+x = 5
+x >>= 3
+print(x)
+
+x = 5
+x <<= 3
+print(x)
+
+print("Comparison Operators")
+
+a=int(input("A="))
+b=int(input("B="))
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
